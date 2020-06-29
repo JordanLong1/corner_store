1e9a6c4211ab6324ddf2179136ab323fd3da4a31
Jordan <jordanleelong5@gmail.com>
2020-06-28 17:01:15 -0700

    building controller out, strong params

--- corner-store-backend/app/controllers/products_controller.rb
+++ corner-store-backend/app/controllers/products_controller.rb
@@ -1,2 +1,21 @@
 class ProductsController < ApplicationController
+
+    def create 
+        product = Product.create(products_params)
+    end
+
+    def show 
+
+    end
+
+    def index 
+
+    end
+
+    private 
+
+    def products_params 
+        params.require(:products).require(:title, :description, :price, :quantity)
+    end
+
 end
