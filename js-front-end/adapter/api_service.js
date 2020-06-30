c9b91d8718a594782f2e9eb001c28ce2e94fa265
Jordan <jordanleelong5@gmail.com>
2020-06-29 21:41:27 -0700

    fetched all products correctly

--- js-front-end/adapter/api_service.js
+++ js-front-end/adapter/api_service.js
@@ -1,1 +1,18 @@
-// fetchs
+// fetch 
+
+class ApiService {
+    constructor() {
+
+        this.baseUrl = "http://localhost:3000" 
+    }
+
+    getProducts() {
+        return fetch(`${this.baseUrl}/products`) 
+        .then(resp => resp.json())
+    }
+
+
+
+}
+
+
