a3fdbab3ea83379464da4cc748c3a04e09921866
Jordan <jordanleelong5@gmail.com>
2020-06-29 19:34:33 -0700

    seed data working

--- corner-store-backend/db/seeds.rb
+++ corner-store-backend/db/seeds.rb
@@ -5,26 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cart.create([{item_count: 0, total_price: 0.0}])
 
 Product.create([
-    {title: "Pink Flamingo Blast", description: "5.5 Inch seam with elastic waistband, Stretch mesh linen for comfort with back pocket zipper", price: 69.99, quantity: 20, category: "Bottoms"}
-    # {title: "Green Gator Takeover", description: "5.5 Inch Stretch swim trunks, Made out of 4-way stretch Polyestor/Spandex Blend. Elastic waistband for fit, Stretch mesh liner for comfort, and a back pocket zipper for secure storage.", price: 49.99, quantity: 20, category: "Bottoms"},
-    # {title: "The Doggie Paddles", description: "5.5 inch Stretch seam. Made out of our 4-way stretch Polyester/Spandex Blend. They are impossibly stretchy.", price: 74.99, quantity: 20, category: "Bottoms"},
-    # {title: "Shark Attack", description: "5.5 Inch seam made out of our 4-way stretch Polyester/Spandex Blend. They are impossibly stretchy.", price: 49.99, quantity: 20, category: "Bottoms"},
-    # {title: "Neon Glow 3000", description: "5.5 Inch seam made out of our 4-way stretch Polyester/Spandex Blend. They are impossibly stretchy.", price: 29.99, quantity: 20, category: "Bottoms"},
-    # {title: "Mens Geo Sails", description: "Long sleeve pocket tee. Fabrics are 100% Cotton. Classic fit with graphic on chest pocket and back.", price: 24.99, quantity: 20, category: "Tops"},
-    # {title: "Mens Bus & Board", description: "Long sleeve pocket tee. Fabrics are 100% Cotton. Classic fit with graphic on chest pocket and back.", price: 29.99, quantity: 20, category: "Tops"},
-    # {title: "Mens Batter-Up!", description: "Short sleeve pocket tee. Fabrics are 100% Cotton. Classic fit with graphic on chest pocket and back.", price: 19.99, quantity: 20, category: "Tops"},
-    # {title: "Mens Lax Brah", description: "Short sleeve pocket tee. Fabrics are 100% Cotton. Classic fit with graphic on chest pocket and back.", price: 19.99, quantity: 20, category: "Tops"},
-    # {title: "Mens Tequila Sunrise", description: "Long sleeve pocket tee. Fabrics are 100% Cotton. Classic fit with graphic on chest pocket and back.", price: 39.99, quantity: 20, category: "Tops"},
-    # {title: "Linen V-Neck Tee", description: "Your basic tee, upgraded. Luxe and lightweight, our linen is the perfect weight for warm-weather layering.", price: 49.99, quantity: 20, category: "Womens"},
-    # {title: "Striped V-Neck Maxi Dress", description: "Getting ready just got so much easier with this chic v-neck maxi dress. Made with a soft Pima cotton/stretch blend fabric and flattering waist tie for either the front or the back, this versatile dress is perfect for work and weekend getaways.", price: 140.00, quantity: 20, category: "Womens"},
-    # {title: "Stripe Peplum Top", description: "Sleeveless peplum top, Yarn dye seersucker stripe, Keyhole at back neck, Body length: 25 inches from the top of the shoulder to the bottom hem.", price: 50.00, quantity: 20, category: "Womens"},
-    # {title: "Dip Dye Vintage Tee", description: "Short-sleeve vintage whale pocket tee. Classic t-shirt silhouette. Dip dye with Vintage whale graphic on front chest pocket and center back.", price: 39.99, quantity: 20, category: "Womens"},
-    # {title: "Floral FLamingo Flip-FLops", description: "Printed footbed, Solid strap with gold whale detailing, Perfect for a day by the water", price: 29.99, quantity: 20, category: "Womens"}
+    {title: "Pink Flamingo Blast", description: "5.5 Inch seam with elastic waistband, Stretch mesh linen for comfort with back pocket zipper", price: 69.99, quantity: 20, category: "Bottoms", cart_id: Cart.first.id},
+    {title: "Green Gator Takeover", description: "5.5 Inch Stretch swim trunks, Made out of 4-way stretch Polyestor/Spandex Blend. Elastic waistband for fit, Stretch mesh liner for comfort, and a back pocket zipper for secure storage.", price: 49.99, quantity: 20, category: "Bottoms", cart_id: Cart.first.id},
+    {title: "The Doggie Paddles", description: "5.5 inch Stretch seam. Made out of our 4-way stretch Polyester/Spandex Blend. They are impossibly stretchy.", price: 74.99, quantity: 20, category: "Bottoms", cart_id: Cart.first.id},
+    {title: "Shark Attack", description: "5.5 Inch seam made out of our 4-way stretch Polyester/Spandex Blend. They are impossibly stretchy.", price: 49.99, quantity: 20, category: "Bottoms", cart_id: Cart.first.id},
+    {title: "Neon Glow 3000", description: "5.5 Inch seam made out of our 4-way stretch Polyester/Spandex Blend. They are impossibly stretchy.", price: 29.99, quantity: 20, category: "Bottoms", cart_id: Cart.first.id},
+    {title: "Mens Geo Sails", description: "Long sleeve pocket tee. Fabrics are 100% Cotton. Classic fit with graphic on chest pocket and back.", price: 24.99, quantity: 20, category: "Tops", cart_id: Cart.first.id},
+    {title: "Mens Bus & Board", description: "Long sleeve pocket tee. Fabrics are 100% Cotton. Classic fit with graphic on chest pocket and back.", price: 29.99, quantity: 20, category: "Tops", cart_id: Cart.first.id},
+    {title: "Mens Batter-Up!", description: "Short sleeve pocket tee. Fabrics are 100% Cotton. Classic fit with graphic on chest pocket and back.", price: 19.99, quantity: 20, category: "Tops", cart_id: Cart.first.id},
+    {title: "Mens Lax Brah", description: "Short sleeve pocket tee. Fabrics are 100% Cotton. Classic fit with graphic on chest pocket and back.", price: 19.99, quantity: 20, category: "Tops", cart_id: Cart.first.id},
+    {title: "Mens Tequila Sunrise", description: "Long sleeve pocket tee. Fabrics are 100% Cotton. Classic fit with graphic on chest pocket and back.", price: 39.99, quantity: 20, category: "Tops", cart_id: Cart.first.id},
+    {title: "Linen V-Neck Tee", description: "Your basic tee, upgraded. Luxe and lightweight, our linen is the perfect weight for warm-weather layering.", price: 49.99, quantity: 20, category: "Womens", cart_id: Cart.first.id},
+    {title: "Striped V-Neck Maxi Dress", description: "Getting ready just got so much easier with this chic v-neck maxi dress. Made with a soft Pima cotton/stretch blend fabric and flattering waist tie for either the front or the back, this versatile dress is perfect for work and weekend getaways.", price: 140.00, quantity: 20, category: "Womens", cart_id: Cart.first.id},
+    {title: "Stripe Peplum Top", description: "Sleeveless peplum top, Yarn dye seersucker stripe, Keyhole at back neck, Body length: 25 inches from the top of the shoulder to the bottom hem.", price: 50.00, quantity: 20, category: "Womens", cart_id: Cart.first.id},
+    {title: "Dip Dye Vintage Tee", description: "Short-sleeve vintage whale pocket tee. Classic t-shirt silhouette. Dip dye with Vintage whale graphic on front chest pocket and center back.", price: 39.99, quantity: 20, category: "Womens", cart_id: Cart.first.id},
+    {title: "Floral FLamingo Flip-FLops", description: "Printed footbed, Solid strap with gold whale detailing, Perfect for a day by the water", price: 29.99, quantity: 20, category: "Womens", cart_id: Cart.first.id}
 
 
 ])
+
+
 
 
 
