9b15ac2d36e5ee84b1944b0f53cf5e0180d9022f
Jordan <jordanleelong5@gmail.com>
2020-06-29 20:18:43 -0700

    added actions, render'ed json in actions

--- corner-store-backend/app/controllers/products_controller.rb
+++ corner-store-backend/app/controllers/products_controller.rb
@@ -1,22 +1,24 @@
 class ProductsController < ApplicationController
 
-    def create 
-        # product = Product.create(products_params)
+    # def create 
+    #     # product = Product.create(products_params)
+    # end
+    def index 
+        products = Product.all 
+        render json: products
     end
 
     def show 
-
+        product = Product.find_by(id: params[:id])
+        render json: product
     end
 
-    def index 
-        producs = Product.all 
-        render: 
-    end
+
 
     private 
 
     def products_params 
-        params.require(:products).require(:title, :description, :price, :quantity)
+        params.require(:products).require(:title, :description, :price, :quantity, :cart_id)
     end
 
 
