75511ce4e7363b04cb88db0c5b3488e14b01c127
Jordan <jordanleelong5@gmail.com>
2020-06-29 21:42:08 -0700

    added constructor

--- js-front-end/components/products.js
+++ js-front-end/components/products.js
@@ -1,1 +1,13 @@
 // products clazz
+
+class Products {
+    constructor(title, description, price, quantity, category, cart_id) {
+        this.title = title; 
+        this.description = description;
+        this.price = price; 
+        this.quantity = quantity;
+        this.category = category;
+        this.cartId = cart_id;
+
+    }
+}
