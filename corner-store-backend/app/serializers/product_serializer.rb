f400ffedfc7d1c5513bbcf5dee0886d665bd2929
Jordan <jordanleelong5@gmail.com>
2020-06-30 20:34:43 -0700

    added id to serializer

--- corner-store-backend/app/serializers/product_serializer.rb
+++ corner-store-backend/app/serializers/product_serializer.rb
@@ -1,4 +1,4 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :price, :quantity, :category
+  attributes :id, :title, :description, :price, :quantity, :category, :id
   has_one :cart
 end
