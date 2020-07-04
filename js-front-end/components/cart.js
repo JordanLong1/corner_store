56f69df9cb7c4ecf5ea10a9f60a1d83c0f90be3d
Jordan <jordanleelong5@gmail.com>
2020-07-03 23:24:23 -0700

    allow constructor to accept objects

--- js-front-end/components/cart.js
+++ js-front-end/components/cart.js
@@ -1,1 +1,11 @@
 // cart class 
+
+class Cart {
+    constructor({item_count, total_price, id}) {
+        this.itemCount = item_count;
+        this.totalPrice = total_price;
+        this.id = id;
+    }
+
+    
+}
