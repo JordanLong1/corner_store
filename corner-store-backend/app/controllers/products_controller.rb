7032000dffc4a7bd53e3a8ea4abb44d6260b98d7
Jordan <jordanleelong5@gmail.com>
2020-07-03 23:23:27 -0700

    fixed typo in products params

--- corner-store-backend/app/controllers/products_controller.rb
+++ corner-store-backend/app/controllers/products_controller.rb
@@ -18,7 +18,7 @@ class ProductsController < ApplicationController
     private 
 
     def products_params 
-        params.require(:products).require(:title, :description, :price, :quantity, :cart_id, :category, :id)
+        params.require(:products).permit(:title, :description, :price, :quantity, :cart_id, :category, :id)
     end
 
 
