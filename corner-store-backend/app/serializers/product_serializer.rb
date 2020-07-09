8374c8f1cc8ab8fd38c16dff7ec72f9096634a41
Jordan <jordanleelong5@gmail.com>
2020-07-08 19:39:33 -0700

    changed belongs to, to has many

--- corner-store-backend/app/serializers/product_serializer.rb
+++ corner-store-backend/app/serializers/product_serializer.rb
@@ -1,4 +1,4 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :price, :quantity, :category, :id, :cart_id
-  has_one :cart
+  attributes :id, :title, :description, :price, :quantity, :category
+  has_many :carts
 end
