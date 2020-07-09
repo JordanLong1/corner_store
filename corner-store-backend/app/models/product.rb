f992fede2a956cc2124f3b572b1ec763414d388a
Jordan <jordanleelong5@gmail.com>
2020-07-08 19:38:54 -0700

    changed association and added class method to get unique names and sort them by id in the render

--- corner-store-backend/app/models/product.rb
+++ corner-store-backend/app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
-  belongs_to :cart, optional: true
+  # belongs_to :cart, optional: true
+    has_and_belongs_to_many :carts
+
+    def self.unique_titles(title)
+      self.find_by(title: title)
+    end
+
 end
