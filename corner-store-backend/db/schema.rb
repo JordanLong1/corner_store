ef3064d9c6c434edae85ca80416f03a1e4eedd87
Jordan <jordanleelong5@gmail.com>
2020-07-11 18:54:49 -0700

    took quantity off of products

--- corner-store-backend/db/schema.rb
+++ corner-store-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_011325) do
+ActiveRecord::Schema.define(version: 2020_07_12_012408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2020_07_09_011325) do
     t.string "title"
     t.string "description"
     t.float "price"
-    t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "category"
