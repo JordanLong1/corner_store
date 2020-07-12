c36b49cfb2b67925638a131fc28401cae2e573a6
Jordan <jordanleelong5@gmail.com>
2020-07-11 18:54:17 -0700

    took out unique_names method

--- corner-store-backend/app/models/product.rb
+++ corner-store-backend/app/models/product.rb
@@ -2,8 +2,6 @@ class Product < ApplicationRecord
   # belongs_to :cart, optional: true
     has_and_belongs_to_many :carts
 
-    def self.unique_titles(title)
-      self.find_by(title: title)
-    end
+    
 
 end
