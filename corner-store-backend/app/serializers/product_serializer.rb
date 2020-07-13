7f276a5b4ab6320a7601778181f195ad80c2cf64
Jordan <jordanleelong5@gmail.com>
2020-07-12 20:29:59 -0700

    took quantity out of serializer

--- corner-store-backend/app/serializers/product_serializer.rb
+++ corner-store-backend/app/serializers/product_serializer.rb
@@ -1,4 +1,4 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :price, :quantity, :category
+  attributes :id, :title, :description, :price, :category
   has_many :carts
 end
